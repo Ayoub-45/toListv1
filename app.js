bfd9a1e590c425496ce810735e4e3a14a8d1fe11
unknown <ayoubnasraoui161@yahoo.fr>
2023-07-15 18:15:16 +0200

    finish the project

--- app.js
+++ app.js
@@ -1,42 +1,133 @@
 const bodyParser=require("body-parser");
 const express=require("express");
 const app=express();
-const date=require(__dirname+"/date.js")
+const mongoose=require("mongoose");
+const _=require("lodash");
+const url="mongodb://127.0.0.1:27017/todolistDB"
+async function updateData(schema,filter,update){
+    try{
+       data=await schema.findOneAndUpdate(filter,update);
+       console.log("data successfully updated!",data)
+    }
+    catch(e){
+        console.log(err)
+    }
+}
+async function findAllData(schema){
+    try{
+      const data=await schema.find({})
+      return data
+      /*data.forEach(dt=>{
+        console.log(dt)
+      })*/
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+  async function find(schema,condition){
+    const data=await schema.findOne({name:condition}).exec()
+    return data
+
+  }
+
+  async function deleteByID(schema,id){
+    const data=await schema.findOneAndDelete({_id:id})
+    console.log("data deleted successfully",data)
+  }
+try{
+    mongoose.connect(url)
+    console.log("Successfully connected .")
+}
+catch(e){
+    console.log(e)
+}
+const itemsSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    }
+});
+const listSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    items:[itemsSchema]
+})
+const List=mongoose.model("list",listSchema);
+const Item=mongoose.model("Item",itemsSchema)
+const item1=new Item({
+    name:"welcome to your todolist"
+});
+const item2=new Item({
+    name:"hit the + button to add item"
+});
+const item3=new Item({
+    name:"<-- hit to delete one"
+});
+const defaultItems=[item1,item2,item3]
 app.set("view engine","ejs");
-let tasks=[];
-let workItems=[]
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
-app.get('/',function(request,response){
-    const day=date();
-    response.render("list",{listTitle:day,listOfTasks:tasks});
-
-});
-app.post("/",function(req,res){
-    const task=req.body.task;
-    if(req.body.list==="Work"){
-        workItems.push(task);
-        res.redirect("/work")
+app.get('/',async function(request,response){
+    const items=await findAllData(Item)
+    if (items.length===0){
+        Item.insertMany(defaultItems);
     }
     else{
-        tasks.push(task)
-        res.redirect("/");
+        response.render("list",{listTitle:"Today",listOfTasks:items});
+    }
+
+});
+app.post("/",async function(req,res){
+    const itemName=req.body.task;
+    const listName=req.body.list;
+    const item=new Item({
+        name:itemName
+    })
+    if(listName==="Today"){
+        item.save()
+        res.redirect('/')
+    }
+    else{
+        data=await find(List,listName)
+      
+        data.items.push(item);
+        data.save(); 
+        console.log("data saved")
+        res.redirect("/"+listName)
     }
     })
+app.post("/delete",function(req,res){
+    const checkedItemId=req.body.checkbox
+    const listName=req.body.listName;
+    if(listName === "Today"){
+        deleteByID(Item,checkedItemId)
+        res.redirect("/")
+    }
+    else{
+        updateData(List,{name:listName},{$pull:{items:{_id:checkedItemId}}})
+        res.redirect("/"+listName)
+    }
 
-app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work list",listOfTasks:workItems })
 })
-app.get("/about",function(req,res){
-    res.render("about")
+app.get("/:customListName",async function(req,res){
+    const customListName=_.capitalize(req.params.customListName)
+    console.log(customListName)
+    const checklist=await find(List,customListName)
+    if (checklist===null){
+        const list=new List({
+            name:customListName,
+            items:defaultItems
+        });
+        list.save()
+        res.redirect("/"+customListName)
+    }
+    else{
+        res.render("list",{listTitle:checklist.name,listOfTasks:checklist.items})
+    }
 })
-/*
-app.post("/work",function(req,res){
-    let task=req.body.task;
-    workItems.push(task)
-    res.redirect("/work")
-})
-*/
 app.listen(3000,function(){
     console.log("The server is running on port 3000");
 })
